76b4c8491e89bab9512eb81897c9af9516e0b851
amandaelfassy1 <61420264+amandaelfassy1@users.noreply.github.com>
2021-02-28 10:35:31 +0100

    Modification css

--- resources/views/attent.blade.php
+++ resources/views/attent.blade.php
@@ -7,7 +7,7 @@
     <div class="w-screen  flex items-center justify-center bg-gray-100">
         <div class="w-full mx-auto py-16" >
             <div class="bg-white px-7 py-14 my-4 w-2/4 mx-auto shadow rounded-md text-center">
-                <img src="{{asset('time.png')}}" alt="">
+                <img src="{{asset('scuba.jpg')}}" alt="">
                 <h1 class="text-black font-bold mb-6 mt-6">En attente de confirmation</h1>
                 <hr>
                 <h3  class="text-blue font-bold">Pour finaliser votre demande, vous devez confirmer l'adresse mail de votre réservation, depuis le mail reçu.</p></h3>
@@ -19,6 +19,7 @@
 img{
     width: 40%;
    margin : auto;
+   border-radius: 50%
 }
 h1{
     font-size: 30px;
