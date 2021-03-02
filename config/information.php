c80317ce4983f9caba84a61c6394395949e3c149
amandaelfassy1 <61420264+amandaelfassy1@users.noreply.github.com>
2021-03-02 17:09:48 +0100

    Creation api

--- config/information.php
+++ config/information.php
@@ -2,7 +2,7 @@
 return 
 [
     'day' => 'du lundi au vendredi',
-    'Heures' => '09:00-18:00',
+    'time' => '09:00-18:00',
     'name' => 'divingAffluence',
     'adress' =>'Marina de Saint-Gilles, Avant-Port, Local n°5, Saint-Gilles les Bains 97434, La Réunion',
 ];
